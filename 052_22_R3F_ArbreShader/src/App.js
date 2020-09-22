5636e46c7e602cb557d66dccff4c0fcfa7a33a7f
josazar <josazar@gmail.com>
2020-09-22 21:43:14 +0200

    React Three Fiber ! it works finally

--- 052_22_R3F_ArbreShader/src/App.js
+++ 052_22_R3F_ArbreShader/src/App.js
@@ -1,18 +1,16 @@
-import React, { Suspense, useRef, useState } from 'react'
-import { Canvas, useFrame } from 'react-three-fiber'
-import { LeafSystem } from './Components/Leaf'
+import React, { Suspense } from 'react'
+import { Canvas } from 'react-three-fiber'
 import { OrbitControls, Html } from 'drei'
 import { PointsCloudModel } from './Components/PointsCloudModel'
 
 export default function App() {
 	return (
 		<div>
-			<Canvas camera={{ fov: 70, position: [0.5, 2, 5.5] }} colorManagement>
-				<ambientLight intensity={0.2} />
-				<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-				<pointLight position={[10, 10, 10]} />
+			<Canvas camera={{ fov: 70, position: [0.5, 2, 3] }} colorManagement>
+				<ambientLight intensity={0.5} />
+				<spotLight position={[0, 10, 10]} angle={0.15} penumbra={1} />
 				<Suspense fallback={<Html>loading..</Html>}>
-					<PointsCloudModel position={[0, -2, 0]} />
+					<PointsCloudModel position={[0, -3, 0]} />
 				</Suspense>
 				<OrbitControls />
 			</Canvas>
--- 052_22_R3F_ArbreShader/src/Components/PointsCloudModel.js
+++ 052_22_R3F_ArbreShader/src/Components/PointsCloudModel.js
@@ -1,110 +1,87 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import * as THREE from 'three'
-import { useLoader, useThree, useUpdate, useFrame } from 'react-three-fiber'
+import { useLoader, useFrame } from 'react-three-fiber'
 import { PLYLoader } from 'three/examples/jsm/loaders/PLYLoader.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { fragmentShader } from './shader/fragment.js'
 import { vertexShader } from './shader/vertex.js'
 
+// Models
 import arbre from './Models/Arbre_Liane_2.ply'
-import scene from './Models/scene-processed.glb'
-import water from './textures/water.jpg'
+import arbreDuo from './Models/Arbre_Duo.ply'
 
-function PointsCloudModel(props) {
-	const geometry = useLoader(PLYLoader, arbre)
-	// const scene = useThree()
-	// avec <suspense></suspense> il ne lit pas la suite ?
+export const PointsCloudModel = (props) => {
+	const ref = useRef()
+	const geometryA = useLoader(PLYLoader, arbre)
+	const geometryB = useLoader(PLYLoader, arbreDuo)
+	// avec <suspense></suspense> Il attend que les assets soient bien loadé
 
 	let material = new THREE.ShaderMaterial({
-		// extensions: {
-		// 	derivatives: '#extension GL_OES_standard_derivatives : enable',
-		// },
 		uniforms: {
 			u_time: { type: 'f', value: 0 },
 			u_lerp: { type: 'f', value: 0 },
 			u_swirlStep: { type: 'f', value: 0 },
-			u_resolution: { type: 'v4', value: new THREE.Vector4() },
-			uvRate1: {
-				value: new THREE.Vector2(1, 1),
-			},
 		},
 		vertexShader: vertexShader,
 		fragmentShader: fragmentShader,
 		transparent: true,
-		morphTargets: true,
 	})
 
-	let number = geometry.attributes.color.count
-	let sizes = new Float32Array(number)
-	for (let i = 0; i < number; i++) {
-		sizes.set([Math.random()], i)
-	}
-	let colors = new Float32Array(number * 3)
-	let colorsA = new Float32Array(number * 3)
-	let positionA = new Float32Array(number * 3)
-	const indices = new Float32Array(number)
-	const speeds = new Float32Array(number)
-	const phytoncides = new Float32Array(number)
+	let number = geometryA.attributes.color.count
+	let colorsB = new Float32Array(number * 3)
+	let positionB = new Float32Array(number * 3)
+	let args = new Float32Array(number * 4) // (pindex, speeds, sizes)
 
 	for (let i = 0; i < number; i++) {
-		colors.set(
+		// colors.set(
+		// 	[
+		// 		geometryA.attributes.color.array[i * 3],
+		// 		geometryA.attributes.color.array[i * 3 + 1],
+		// 		geometryA.attributes.color.array[i * 3 + 2],
+		// 	],
+		// 	i * 3
+		// )
+		colorsB.set(
 			[
-				geometry.attributes.color.array[i * 3],
-				geometry.attributes.color.array[i * 3 + 1],
-				geometry.attributes.color.array[i * 3 + 2],
+				geometryB.attributes.color.array[i * 3],
+				geometryB.attributes.color.array[i * 3 + 1],
+				geometryB.attributes.color.array[i * 3 + 2],
 			],
 			i * 3
 		)
-		colorsA.set(
+		positionB.set(
 			[
-				geometry.attributes.color.array[i * 3],
-				geometry.attributes.color.array[i * 3 + 1],
-				geometry.attributes.color.array[i * 3 + 2],
+				geometryB.attributes.position.array[i * 3],
+				geometryB.attributes.position.array[i * 3 + 1],
+				geometryB.attributes.position.array[i * 3 + 2],
 			],
 			i * 3
 		)
-		positionA.set(
-			[
-				geometry.attributes.position.array[i * 3],
-				geometry.attributes.position.array[i * 3 + 1],
-				geometry.attributes.position.array[i * 3 + 2],
-			],
-			i * 3
-		)
-		indices[i] = i
-		speeds[i] = Math.random()
 
-		if (i % 100 === 0) {
-			phytoncides[i] = 1
-		} else {
-			phytoncides[i] = 0
+		let phy = 0
+		if (i % 200 === 0) {
+			phy = 1
 		}
+		// args = set(pIndex, Speeds, Sizes)
+		args.set([i, Math.random(), Math.random(), phy], i * 4)
 	}
-	geometry.setAttribute('size', new THREE.BufferAttribute(sizes, 1))
-	geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
-	geometry.setAttribute('colorA', new THREE.BufferAttribute(colorsA, 3))
-	geometry.setAttribute('positionA', new THREE.BufferAttribute(positionA, 3))
-	geometry.setAttribute('pindex', new THREE.BufferAttribute(indices, 1))
-	geometry.setAttribute('speed', new THREE.BufferAttribute(speeds, 1))
-	geometry.setAttribute('phytoncide', new THREE.BufferAttribute(phytoncides, 1))
+
+	// geometryA.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+	geometryA.setAttribute('colorB', new THREE.BufferAttribute(colorsB, 3))
+	geometryA.setAttribute('positionB', new THREE.BufferAttribute(positionB, 3))
+	geometryA.setAttribute('args', new THREE.BufferAttribute(args, 4))
+
+	// RAF
+	useFrame(() => {
+		ref.current.material.uniforms.u_time.value += 0.05
+	})
 
 	//  COMPONENT
 	// ******************************************************************************
 	return (
 		<group {...props}>
-			<points geometry={geometry}>
-				{/* <bufferGeometry attach="geometry" geometry={geometry} /> */}
-				{/* <planeBufferGeometry attach="geometry" args={[5, 5]} /> */}
-				{/* <shaderMaterial {...material} /> */}
-				<shaderMaterial
-					attach="material"
-					args={[material]}
-					// uniforms-texture-value={waterTexture}
-					// uniforms-displacement-value={dispImg}
-				/>
+			<points geometry={geometryA} ref={ref}>
+				<shaderMaterial attach="material" args={[material]} />
 			</points>
 		</group>
 	)
 }
-
-export { PointsCloudModel }
