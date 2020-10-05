0ed7fc5040d2342af390b4b17ef68223c8eed43e
josazar <josazar@gmail.com>
2020-10-05 21:58:59 +0200

    shader with texture

--- 065_05_Shader/shader2.glsl
+++ 065_05_Shader/shader2.glsl
@@ -3,8 +3,9 @@
 precision mediump float;
 #endif
 
+uniform sampler2D u_texture_1;
 uniform sampler2D u_texture_2;
-
+uniform sampler2D u_texture_0;
 uniform vec2 u_tex0Resolution;
 
 uniform vec2 u_resolution;
@@ -38,51 +39,24 @@ float box(vec2 _st,vec2 _size){
   return uv.x*uv.y;
 }
 
-// signed distance to a 2D triangle
-float sdTriangle(in vec2 p0,in vec2 p1,in vec2 p2,in vec2 p)
-{
-  vec2 e0=p1-p0;
-  vec2 e1=p2-p1;
-  vec2 e2=p0-p2;
-  
-  vec2 v0=p-p0;
-  vec2 v1=p-p1;
-  vec2 v2=p-p2;
-  
-  vec2 pq0=v0-e0*clamp(dot(v0,e0)/dot(e0,e0),0.,1.);
-  vec2 pq1=v1-e1*clamp(dot(v1,e1)/dot(e1,e1),0.,1.);
-  vec2 pq2=v2-e2*clamp(dot(v2,e2)/dot(e2,e2),0.,1.);
-  
-  float s=sign(e0.x*e2.y-e0.y*e2.x);
-  vec2 d=min(min(vec2(dot(pq0,pq0),s*(v0.x*e0.y-v0.y*e0.x)),
-  vec2(dot(pq1,pq1),s*(v1.x*e1.y-v1.y*e1.x))),
-  vec2(dot(pq2,pq2),s*(v2.x*e2.y-v2.y*e2.x)));
-  
-  return-sqrt(d.x)*sign(d.y);
-}
-
 void main(){
-  vec2 pos=vec2((2.*gl_FragCoord.xy-u_resolution.xy)/u_resolution.xy);
-  
-  float curves=cos(pos.y)*tan(pos.x)*.5;
-  float r=sin(pos.x+u_time)*.5+.5;
+  vec2 pos=vec2((gl_FragCoord.xy-u_resolution.xy)/u_resolution.xy);
+  vec4 texture=texture2D(u_texture_2,pos+1.)*1.;
+  pos+=.5;
+  float curves=cos(pos.y)*cos(pos.x)*.5;
+  float r=sin(pos.y)*.5+.5;
   float g=curves;
   float b=cos(pos.y);
-  vec3 background=vec3(r,g,b);
-  
-  
-  pos.x+=sin(u_time)*.5;
-  pos.y+=cos(u_time)*.5;
+  texture.rg=rotate2D(vec2(texture),u_time);
+  vec3 background=vec3(g*2.,texture.g,curves);
+  pos.x+=sin(u_time)*.15;
+  pos.y+=cos(u_time)*.15;
   pos*=sin(loopease(.5)*2.)*.5+.5;
   pos+=.5;
   pos=rotate2D(pos,u_time);
-  float bx=box(pos,vec2(.45,.25));
-  float bx2=box(pos,vec2(.43,.23));
-  
-  // color = mix(color, vec3(box1), .5);
+  float bx=box(pos,vec2(.25,.25));
+  float bx2=box(pos,vec2(.24,.24));
   vec3 color=(bx<=0.)?background:vec3(1.,1.,1.);
-  //color-= vec3(bx2);
-  color=(bx2<=0.)?color:vec3(.1961,.1294,.2275);
-  
+  color=(bx2<=0.)?color:vec3(texture.rgb);
   gl_FragColor+=vec4(color,1.);
 }
