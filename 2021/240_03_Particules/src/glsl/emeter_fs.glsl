2f83689e8fb2008fb14b2a8604b96aad79e45683
Joseph AZAR <j.azar@par-azar.com>
2021-08-03 22:16:17 +0200

    Update emeter_fs.glsl

--- 2021/240_03_Particules/src/glsl/emeter_fs.glsl
+++ 2021/240_03_Particules/src/glsl/emeter_fs.glsl
@@ -12,17 +12,19 @@ float circle(in vec2 _st,in float _radius){
 }
 
 void main(){
-  
+
   float dist=length(gl_PointCoord-vec2(.5));
   float disc=smoothstep(.5,.49,dist);
   vec3 col=vColor;
   col*=1.2;
-  
+
+  vec3 colorA = vec3(0.149,0.141,0.912);
+
   // color circle
   // col+=vec3(circle(vPos.xy-vMouse,.25));
-  
+
   // col+=vec3(vPos.x*.25,.0,.84);
-  
+
   if(col.z>disc)discard;
   gl_FragColor=vec4(col,1.);
 }
