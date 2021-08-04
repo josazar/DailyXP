01e42e8902352e3ede88580d4724ee167445a1e6
josazar <josazar@gmail.com>
2021-08-04 15:17:06 +0200

    Update paint_fs.glsl

--- 2021/241_04_particles/src/glsl/paint_fs.glsl
+++ 2021/241_04_particles/src/glsl/paint_fs.glsl
@@ -5,20 +5,9 @@ uniform float PR;
 
 varying vec2 vUv;
 
-
-
-float circle(in vec2 _st, in float _radius){
-    vec2 dist = _st-vec2(0.5);
-	return 1.-smoothstep(_radius-(_radius*0.01),
-                         _radius+(_radius*0.01),
-                         dot(dist,dist)*4.0);
-}
 void main(){
-  // vec2 st = gl_FragCoord.xy/u_resolution.xy;
-  // st.x *= u_resolution.x/u_resolution.y;
-
-  vec3 color = vec3(circle(-uMouse + vUv,0.01));
-  color += vec3(vUv.x, vUv.y, 0.);
-
+  
+  vec3 color=vec3(vUv.x,vUv.y,0.);
+  
   gl_FragColor=vec4(color,1.);
 }
