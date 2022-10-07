48ebe1d6f718911b27690343fbe33e5b5833f6e8
Joseph AZAR <josazar@gmail.com>
2022-10-07 19:46:41 +0200

    Update

---
+++ 2022/lidar-gpgpu-elastik/src/store.js
@@ -0,0 +1,41 @@
+import create from "zustand";
+import { Vector2 } from "three";
+import toast from "react-hot-toast";
+import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader.js";
+
+const useStore = create((set, get) => ({
+  // States
+  mouseMoved: false,
+  mouseCoords: new Vector2(),
+  activeGeometry: null,
+
+  // Actions
+  setMouseMove: (bool) => {
+    set({ mouseMoved: bool });
+  },
+  setMouseCoords: ({ x, y }) => {
+    get().mouseCoords.set(x, y);
+  },
+  loadPlyFile: (url) => {
+    const loader = new PLYLoader();
+
+    toast.loading("Loading...", {
+      position: "bottom-center"
+    });
+
+    loader.load(
+      url,
+      (geometry) => {
+        geometry.center();
+        set({ activeGeometry: geometry });
+        setTimeout(() => {
+          toast.dismiss();
+        }, 500);
+      },
+      (error) => {
+      }
+    );
+  }
+}));
+
+export default useStore;
